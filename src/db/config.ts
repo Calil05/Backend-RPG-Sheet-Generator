9b41d2b023da22f9e615b2e4f42410781f6cdb34
Gabriel Calil <gabrielbcalil@gmail.com>
2024-04-14 16:27:38 -0300

    feat: add dotenv

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,12 +1,14 @@
 import { Sequelize } from "sequelize-typescript";
 import { User } from "../models/userModel";
+import * as dotenv from 'dotenv'; 
+dotenv.config()
 
 const connection = new Sequelize({
     dialect:"mysql",
-    host:"localhost",
-    username:"root",
-    password:"root",
-    database:"rpgsheetgenerator",
+    host:process.env.DB_HOST,
+    username:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_DATABASE,
     logging:false,
     models:[ User ]
 });
